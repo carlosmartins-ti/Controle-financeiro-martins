244f4090258076cfb284b9c343264c177928d2e5
carlos martins <cr954779@gmail.com>
2026-02-02 09:21:49 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,20 @@
 import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL não definida")
 
+
 def get_connection():
     return psycopg2.connect(
         DATABASE_URL,
-        sslmode="require"
+        sslmode="require",
+        cursor_factory=RealDictCursor
     )
+
 
 def init_db():
     conn = get_connection()
